441c6190cd41ea6b346bdcf10819a619aaee2823
Viktoriya(KochurovaVA) <kochurovaviktoriya@mail.ru>
2020-06-01 15:24:09 +0300

    code: исправления2

--- main.cpp
+++ main.cpp
@@ -1,110 +1,65 @@
+#include "histogram.h"
+#include "svg_module.h"
 #include <iostream>
 #include <vector>
 #include <string>
-#include <cmath>
-#include "histogram.h"
-#include "svg_module.h"
 
 using namespace std;
 
-const size_t SCREEN_WIDTH = 80;
-const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
-
-vector<double> input_numbers(size_t count)
-{
+vector<double> input_numbers(size_t count) {
     vector<double> result(count);
-    for (size_t i = 0; i < count; i++)
-    {
+    for (size_t i = 0; i < count; i++) {
         cin >> result[i];
     }
     return result;
 }
 
-vector <size_t> make_histogram(const vector<double> & numbers, size_t bin_count, double min, double max)
-{
-    find_minmax(numbers,min,max);
-    vector<size_t> bins(bin_count,0);
-    for (double number : numbers)
-    {
-        size_t bin;
-        bin = (number - min) / (max - min) * bin_count;
-        if (bin == bin_count)
-        {
+vector<size_t> make_histogram(const vector<double> & numbers, size_t bin_count, double min, double max) {
+    vector<size_t> bins(bin_count);
+    for (double number : numbers) {
+        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
+        if (bin == bin_count) {
             bin--;
         }
         bins[bin]++;
     }
-    return bins;
+    return(bins);
 }
 
-void make_histogram(const vector<double>& numbers, double& max, double& min, size_t bin_count, vector <size_t>& bins )
-{
+void show_histogram_text(const vector<size_t>& bins) {
+    const size_t SCREEN_WIDTH = 80;
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
 
-    for(double x: numbers)
-    {
-        size_t bin_index=(x-min)/(max-min)*bin_count;
-        if (bin_index==bin_count)
-        {
-            bin_index--;
+    size_t max_count = 0;
+    for (size_t count : bins) {
+        if (count > max_count) {
+            max_count = count;
         }
-        bins[bin_index]++;
+    }
+    const bool scaling_needed = max_count > MAX_ASTERISK;
+
+    for (size_t bin : bins) {
+        if (bin < 100) {
+            cout << ' ';
+        }
+        if (bin < 10) {
+            cout << ' ';
+        }
+        cout << bin << "|";
+
+        size_t height = bin;
+        if (scaling_needed) {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            height = (size_t)(bin * scaling_factor);
+        }
+
+        for (size_t i = 0; i < height; i++) {
+            cout << '*';
+        }
+        cout << '\n';
     }
 }
-void show_histogram_text(vector <size_t>& X)
-{
-    size_t max_bin = X[0];
-    for (size_t bin:X)
-    {
-        if (bin > max_bin)
-        {
-            max_bin=bin;
-        }
-    }
-    if (max_bin > MAX_ASTERISK)
-    {
-        double factor = MAX_ASTERISK/static_cast<double>(max_bin);
-        for (size_t bin:X)
-        {
-            if (bin <100)
-            {
-                cout<<" ";
-                if (bin < 10)
-                {
-                    cout<<" ";
-                }
-            }
-            cout <<bin <<"|";
 
-            size_t height = bin*factor;
-            for(int i=0; i<height; i++)
-            {
-                cout<< "*";
-            }
-            cout<<endl;
-        }
-    }
-
-    else
-    {
-        for (size_t bin:X)
-        {
-            if (bin <100)
-            {
-                cout<<" ";
-                if(bin < 10)
-                {
-                    cout<<" ";
-                }
-            }
-        cout<<bin<<"|";
-        for (int i = 0; i < bin; i++)
-        {
-            cout << "*";
-        }
-        cout<<endl;
-        }
-    }
-}
 int main() {
 
     size_t number_count;
@@ -130,12 +85,11 @@ int main() {
 	}
 
     double min, max;
-
-    find_minmax(numbers, min, max);
-    const auto bins = make_histogram(numbers, min, max, bin_count);
+    const auto bins = make_histogram(numbers, bin_count, min, max);
 
     show_histogram_svg(bins, colors);
 
     }
     return 0;
 }
+
