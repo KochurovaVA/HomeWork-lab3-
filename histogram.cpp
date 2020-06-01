7bc0fbf24ebc6f309b57eec2cbb55b1f7fc5b880
Viktoriya(KochurovaVA) <kochurovaviktoriya@mail.ru>
2020-06-01 15:28:43 +0300

    code: исправления2

--- histogram.cpp
+++ histogram.cpp
@@ -1,18 +1,19 @@
-#include "histogram.h"
+#include <vector>
+#include <iostream>
 
-void find_minmax(const vector<double>& numbers, double& min, double& max)
-{
-    min = numbers[0];
-    max = numbers[0];
-    for (double number : numbers)
-    {
-        if (number < min)
-        {
-            min = number;
-        }
-        if (number > max)
-        {
-            max = number;
+using namespace std;
+
+void find_minmax(const vector<double> & numbers, double& min, double& max) {
+    if (numbers.size()) {
+        min = numbers[0];
+        max = numbers[0];
+        for (double number : numbers) {
+            if (number < min) {
+                min = number;
+            }
+            if (number > max) {
+                max = number;
+            }
         }
     }
 }
